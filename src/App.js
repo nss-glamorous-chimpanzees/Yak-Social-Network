e082a115e2925f6239658d31fa1227db73c10e04
joshdbarton <joshdbarton@gmail.com>
2018-05-31 11:26:41 -0500

    added FriendList and Friend Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import FriendList from './friends/FriendList';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       Chimps be Glam!
+        <div id="friends-div">
+          <FriendList userId={this.state.userId}/>
+        </div>
       </div>
     );
   }
---
+++ src/friends/FriendList.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import Friend from './Friend';
+import "./FriendList.css"
+
+class FriendList extends Component {
+    constructor (props) {
+        super(props)
+        this.UniqueKey = 1
+        this.state = {
+            friendList: []
+        }
+
+    }
+
+    loadFriends() {
+        let allUsers
+        fetch(("http://localhost:8088/users"))
+            .then(users => users.json())
+            .then(users => {
+                allUsers = users
+                return fetch(`http://localhost:8088/users/?id=${this.props.userId}&_embed=follows`)
+            })
+            .then(user => user.json())
+            .then(userWithFollows => {
+            const friendfollows = userWithFollows[0].follows.filter(follow => follow.friends)
+            const friends = []
+            friendfollows.forEach(friendfollow => {
+                friends.push(allUsers.find(user => user.id === friendfollow.followId))
+            })
+            return friends
+        })
+        .then(friends => {
+            this.setState({
+                friendList: friends
+            })
+        })
+    }
+
+    componentDidMount () {
+        this.loadFriends()
+    }
+
+    render () {
+        return (
+            <div id="friend-list">
+                {this.state.friendList.map(friend => (
+                    <Friend first={friend.first} last={friend.last} location={friend.location} img={friend.image} />
+                ))}
+            </div>
+        )
+    }
+}
+
+export default FriendList
