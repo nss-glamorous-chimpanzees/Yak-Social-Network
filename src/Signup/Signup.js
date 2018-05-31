8a4cd1ae87e30dc0e697d70876d4ec05acf65352
pzc <paul.zimmerman.clayton@gmail.com>
2018-05-31 15:31:05 -0500

    css connected, added dropdown for location

--- src/Signup/Signup.js
+++ src/Signup/Signup.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./Signup.css";
 
 class Signup extends Component {
   constructor(props) {
@@ -6,12 +7,14 @@ class Signup extends Component {
     this.state = {
       firstname: "",
       lastname: "",
-      email: ""
+      email: "",
+      location: ""
     };
 
     this.firstnameChange = this.firstnameChange.bind(this);
     this.lastnameChange = this.lastnameChange.bind(this);
     this.emailChange = this.emailChange.bind(this);
+    this.locationChange = this.locationChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -26,43 +29,66 @@ class Signup extends Component {
     this.setState({ email: event.target.value });
   }
 
+  locationChange(event) {
+    this.setState({ location: event.target.value });
+  }
+
   handleSubmit(event) {
     alert(
       "A name was submitted: " +
-        this.state.firstname + " " + this.state.lastname + " " + this.state.email
+        this.state.firstname +
+        " " +
+        this.state.lastname +
+        " " +
+        this.state.email +
+        " " +
+        this.state.location
     );
     // event.preventDefault()
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          First Name:
-          <input
-            type="text"
-            value={this.state.firstname}
-            onChange={this.firstnameChange}
-          />
-        </label>
-        <label>
-          Last Name:
-          <input
-            type="text"
-            value={this.state.lastname}
-            onChange={this.lastnameChange}
-          />
-        </label>
-        <label>
-          Email:
-          <input
-            type="text"
-            value={this.state.email}
-            onChange={this.emailChange}
-          />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <div className="signup__container">
+        <h1 className="signup__title">Sign-up to use our app below!</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            First Name:
+            <input
+              type="text"
+              value={this.state.firstname}
+              onChange={this.firstnameChange}
+            />
+          </label>
+          <label>
+            Last Name:
+            <input
+              type="text"
+              value={this.state.lastname}
+              onChange={this.lastnameChange}
+            />
+          </label>
+          <label>
+            Email:
+            <input
+              type="text"
+              value={this.state.email}
+              onChange={this.emailChange}
+            />
+          </label>
+          <label>
+            Location:
+            <select value={this.state.location} onChange={this.locationChange}>
+              <option selected value="">Pick one</option>
+              <option value="grapefruit">Grapefruit</option>
+              <option value="lime">Lime</option>
+              <option value="coconut">Coconut</option>
+              <option value="mango">Mango</option>
+            </select>
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     );
   }
 }
